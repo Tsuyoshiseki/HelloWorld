4998611b87bd87b051156b8e7ae3d47ec19427e9
Luã Seki <158064034+Tsuyoshiseki@users.noreply.github.com>
2025-05-14 06:44:07 -0300

    Hello World

---
+++ Java/Olamundo.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package olamundo;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author tsuyo
+ */
+public class Olamundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application lo
+        String meuPrimeiroPrograma;
+        
+        JOptionPane.showMessageDialog(null,"Hello World");
+       
+    }
+
+}
